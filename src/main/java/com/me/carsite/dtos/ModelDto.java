9f542fd97e2d6a30aa664bcea2f79056bbd7c749
Mahmud Ferzeli <mahmudfrzl@mail.ru>
2021-09-23 21:38:19 -0700

    ModelDto added

--- src/main/java/com/me/carsite/dtos/ModelDto.java
+++ src/main/java/com/me/carsite/dtos/ModelDto.java
@@ -1,2 +1,14 @@
-package com.me.carsite.dtos;public class ModelDto {
+package com.me.carsite.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelDto {
+    private Long id;
+    private String name;
+    private Long markaId;
 }
