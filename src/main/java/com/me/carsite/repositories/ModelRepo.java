f2856976c21a91a2bd4dea334af8600f622b9c1f
Mahmud Ferzeli <mahmudfrzl@mail.ru>
2021-09-23 21:40:19 -0700

    Repositories:ModelRepo updated

--- src/main/java/com/me/carsite/repositories/ModelRepo.java
+++ src/main/java/com/me/carsite/repositories/ModelRepo.java
@@ -1,7 +1,16 @@
 package com.me.carsite.repositories;
 
+import com.me.carsite.dtos.ModelDto;
 import com.me.carsite.models.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ModelRepo extends JpaRepository<Model,Long> {
+
+
+    @Query("Select new com.me.carsite.dtos.ModelDto(m.id,m.name,m.marka.id ) From Model m where m.marka.id=:markaId")//marka id-sine gore getir
+    List<ModelDto> getByMarkaId(Long markaId);
 }
