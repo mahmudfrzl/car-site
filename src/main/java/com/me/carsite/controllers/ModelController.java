56181fd99a90cde34393b3fb68500310e65d81e4
Mahmud Ferzeli <mahmudfrzl@mail.ru>
2021-09-23 21:37:22 -0700

    ModelController added

--- src/main/java/com/me/carsite/controllers/ModelController.java
+++ src/main/java/com/me/carsite/controllers/ModelController.java
@@ -1,2 +1,23 @@
-package com.me.carsite.controllers;public class ModelController {
+package com.me.carsite.controllers;
+
+import com.me.carsite.dtos.ModelDto;
+import com.me.carsite.models.Model;
+import com.me.carsite.services.abstracts.ModelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/models")
+@RequiredArgsConstructor
+public class ModelController {
+    private final ModelService modelService;
+
+    @GetMapping("/getByMarkaId")
+    public List<ModelDto> getByMarkaId(@RequestParam Long markaId) {
+        return modelService.getByMarkaId( markaId);
+    }
+
 }
