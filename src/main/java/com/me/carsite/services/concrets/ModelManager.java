dee315ff35de0f6b2d30e7977174b75d24b1ae3a
Mahmud Ferzeli <mahmudfrzl@mail.ru>
2021-09-23 21:44:32 -0700

    Implemented with JPQR

--- src/main/java/com/me/carsite/services/concrets/ModelManager.java
+++ src/main/java/com/me/carsite/services/concrets/ModelManager.java
@@ -1,2 +1,31 @@
-package com.me.carsite.services.concrets;public class ModelManager {
+package com.me.carsite.services.concrets;
+
+import com.me.carsite.dtos.ModelDto;
+import com.me.carsite.models.Model;
+import com.me.carsite.repositories.MarkaRepo;
+import com.me.carsite.repositories.ModelRepo;
+import com.me.carsite.services.abstracts.ModelService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ModelManager implements ModelService {
+    private final ModelRepo modelRepo;
+    private final ModelMapper modelMapper;
+    private final MarkaRepo markaRepo;
+
+
+
+
+    @Override
+    public List<ModelDto> getByMarkaId(Long markaId) {
+        return modelRepo.getByMarkaId(markaId);
+    }
 }
